f95832b01a1eb11a0d73cce65ac08a4d08257de0
Shandy Hermawan <hrm.shandy05@gmail.com>
2024-03-08 07:36:03 +0000

    feat: add button in create user form for better ux

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,49 +1,61 @@
 import { useState } from "react";
 import { uuid } from "@/lib/utils";
-import { Input } from "./ui";
+import { Button, Input } from "./ui";
 import { useUserList } from "@/hooks/useUserList";
 
 const AddUser = () => {
   const { userList, setUserList } = useUserList();
   const [newUser, setNewUser] = useState("");
 
-  const addNewUser = (e) => {
+  const createUser = () => {
+    if (!newUser) {
+      alert("Please enter a user name");
+      return;
+    }
+
+    const trimmedNewUser = newUser.trim();
+    const existingUser = userList.find((user) => user.name === trimmedNewUser);
+
+    if (existingUser) {
+      alert(`${newUser} is already a user.`);
+      return;
+    }
+
+    const user = {
+      id: uuid(),
+      name: trimmedNewUser,
+      followers: [],
+      following: [],
+    };
+    setUserList((prevList) => [...prevList, user]);
+    setNewUser("");
+  };
+
+  const addUserByEnter = (e) => {
     if (e.key === "Enter") {
-      if (!newUser) {
-        alert("Please enter a user name");
-        return;
-      }
-
-      const trimmedNewUser = newUser.trim();
-      const existingUser = userList.find(
-        (user) => user.name === trimmedNewUser,
-      );
-
-      if (existingUser) {
-        alert(`${newUser} is already a user.`);
-        return;
-      }
-
-      const user = {
-        id: uuid(),
-        name: trimmedNewUser,
-        followers: [],
-        following: [],
-      };
-      setUserList((prevList) => [...prevList, user]);
-      setNewUser("");
+      createUser();
     }
   };
 
   return (
-    <div>
+    <div className="flex space-x-2">
       <Input
         type="text"
         placeholder="Enter New User"
         value={newUser}
         onChange={(e) => setNewUser(e.target.value)}
-        onKeyDown={addNewUser}
+        onKeyDown={addUserByEnter}
       />
+      <Button onClick={createUser}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="w-4 h-4"
+        >
+          <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+        </svg>
+      </Button>
     </div>
   );
 };
